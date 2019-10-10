dc0d3d5e45176b369c29254c256614ecf09454a0
Nikola Ljubešić <nljubesi@gmail.com>
2019-10-10 12:32:04 +0000

    First solution

---
+++ prep_entry.py
@@ -0,0 +1,46 @@
+import xml.etree.ElementTree as ET
+import json
+train=[]
+test=[]
+prev_page_id='p1'
+prev_base=0
+labels=[]
+feats=[]
+for i in range(310):
+  if i<200:
+    add=train
+  else:
+    add=test
+  try:
+    tree=ET.parse('tokenizer/correct_entries/entry_'+str(i)+'.xml')
+  except:
+    continue
+  root=tree.getroot()
+  tokens=list(root.iter('TOKEN'))
+  for idx,token in enumerate(tokens):
+    page_id=token.attrib['sid'].split('_')[0]
+    if page_id!=prev_page_id and len(labels)>0:
+      feats[0].append('BOS')
+      feats[-1].append('EOS')
+      add.append((labels,feats))
+      labels=[]
+      feats=[]
+      prev_base=0
+      prev_page_id=page_id
+    if float(token.attrib['base'])!=prev_base:
+      labels.append('NEWLINE')
+      feats.append(['NEWLINE'])
+      prev_base=float(token.attrib['base'])
+    if token.text==None:
+      token.text=''
+    feat=['SIZE='+token.attrib['font-size'],'BOLD='+token.attrib['bold'],'ITALIC='+token.attrib['italic'],'FONT='+token.attrib['font-name'],'TOKEN='+token.text]
+    if idx==0:
+      label='START'
+    elif idx+1==len(tokens):
+      label='INSIDE'
+    else:
+      label='INSIDE'
+    feats.append(feat)
+    labels.append(label)
+json.dump(train,open('entry/train.json','w'),indent=4)
+json.dump(test,open('entry/test.json','w'),indent=4)
---
+++ prep_orth+sense.py
@@ -0,0 +1,72 @@
+import xml.etree.ElementTree as ET
+import json
+train=[]
+test=[]
+def are_related(e1,e2):
+  while True:
+    if e1 in e2:
+      return True
+    if e1 in parent_map:
+      e1=parent_map[e1]
+    else:
+      return False
+    
+for i in range(310):
+  if i<200:
+    add=train
+  else:
+    add=test
+  try:
+    tree=ET.parse('tokenizer/correct_entries/entry_'+str(i)+'.xml')
+  except:
+    continue
+  root=tree.getroot()
+  parent_map={c:p for p in tree.iter() for c in p}
+  tokens=list(root.iter('TOKEN'))
+  orth=None
+  sense=None
+  feats=[]
+  labels=[]
+  prev_base=0
+  for element in root.iter():
+    if element.tag=='orth':
+      orth=element
+    if element.tag=='sense':
+      sense=element
+    if element.tag=='TOKEN':
+      label=None
+      if element!=None:
+        if element in orth:
+          label='ORTH'
+      if sense!=None:
+        if are_related(element,sense):
+          label='SENSE'+sense.attrib.get('n','1')
+      if label==None:
+        label='O'
+      token=element
+      if float(token.attrib['base'])!=prev_base:
+        labels.append('NEWLINE')
+        feats.append(['NEWLINE'])
+        prev_base=float(token.attrib['base'])
+      if token.text==None:
+        token.text=''
+      feat=['SIZE='+token.attrib['font-size'],'BOLD='+token.attrib['bold'],'ITALIC='+token.attrib['italic'],'FONT='+token.attrib['font-name'],'TOKEN='+token.text]
+      feats.append(feat)
+      labels.append(label)
+  feats[0].append('BOS')
+  feats[-1].append('EOS')
+  sense=''
+  for idx,label in enumerate(labels):
+    if label.startswith('SENSE'):
+      if sense!=label:
+        labels[idx]='SENSE_START'
+      else:
+        labels[idx]='SENSE_INSIDE'
+      #if idx+1<len(labels):
+      #  if labels[idx+1]!=label:
+      #    labels[idx]='SENSE_END'
+      sense=label
+  add.append((labels,feats))
+
+json.dump(train,open('orth+sense/train.json','w'),indent=4)
+json.dump(test,open('orth+sense/test.json','w'),indent=4)
