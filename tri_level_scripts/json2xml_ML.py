671fb86051d0beb87dcd54ec1637d2bda211d86e
Jan Jug <john.south@hotmail.com>
2020-01-10 19:47:17 +0100

    json to xml conversion on all 3 levels

--- tri_level_scripts/json2xml_ML.py
+++ tri_level_scripts/json2xml_ML.py
@@ -12,24 +12,27 @@ def json2xml( json_in_file, xml_raw, xml_out_file ):
 
     page_level_tokens = json_data['page_level'][0]
     page_level_labels = json_data['page_level'][1]
-    # entry_level_tokens = json_data['entry_level'][0]
-    # entry_level_labels = json_data['entry_level'][1]
-    # sense_level_tokens = json_data['sense_level'][0]
-    # sense_level_labels = json_data['sense_level'][1]
+    entry_level_tokens = json_data['entry_level'][0]
+    entry_level_labels = json_data['entry_level'][1]
+    sense_level_tokens = json_data['sense_level'][0]
+    sense_level_labels = json_data['sense_level'][1]
 
-    doc_elm = ET.Element( 'document' )
-    body_elm = ET.SubElement( doc_elm, 'body' )
-    cur_elm = body_elm
 
-    label_prev = ""
+    # get label for each token in the raw file
+    token_labels = []
+
     i_p = 0
-    # i_e = 0
-    # i_s = 0
+    i_e = 0
+    i_s = 0
     i_pt = 0
+    i_et = 0
+    i_st = 0
     prev_page = int( tokens_raw[0].attrib['page'] )
     for token_r in tokens_raw:
 
-        if token_r.text is None:        # empty tokens were skipped
+        label_cur = ["", "", ""]
+
+        if token_r.text is None:  # empty tokens were skipped
             continue
 
         cur_page = int( token_r.attrib['page'] )
@@ -39,41 +42,167 @@ def json2xml( json_in_file, xml_raw, xml_out_file ):
             i_pt = 0
             prev_page = cur_page
 
-        token_pl = page_level_tokens[i_p][i_pt]
-        token_pl_text = token_pl[4][6:]
+        token_lvl1 = page_level_tokens[i_p][i_pt]
+        token_lvl1_text = token_lvl1[4][6:]
 
-        if token_r.text != token_pl_text:
+        if token_r.text != token_lvl1_text:
             print( "error! misaligned data!" )
             break
 
-        label_cur = page_level_labels[i_p][i_pt]
+        label_cur[0] = page_level_labels[i_p][i_pt]
 
-        if label_cur == 'SCRAP':
-            if cur_elm.tag == 'container' and cur_elm.attrib['name'] == 'dictScrap':
-                cur_elm.append( token_r )
-            else:
-                scrap_cont = ET.SubElement( cur_elm, 'container', attrib={'name' : 'dictScrap'} )
-                cur_elm = scrap_cont
-                cur_elm.append( token_r )
+        if i_e < len( entry_level_tokens ):
+            token_lvl2 = entry_level_tokens[i_e][i_et]
+            token_lvl2_text = token_lvl2[4][6:]
 
-        elif label_cur == 'ENTRY_START':
-            entry_cont = ET.SubElement( body_elm, 'container', attrib={'name' : 'entry'} )
-            cur_elm = entry_cont
-            cur_elm.append( token_r )
+            if token_r.text == token_lvl2_text:
+                label_cur[1] = entry_level_labels[i_e][i_et]
+                i_et += 1
+                if i_et == len( entry_level_tokens[i_e] ):
+                    i_e += 1
+                    i_et = 0
 
-        elif label_cur == 'ENTRY_INSIDE':
-            if cur_elm.tag != 'container' or (cur_elm.tag == 'container' and cur_elm.attrib['name'] != 'entry'):
-                entry_cont = ET.SubElement( body_elm, 'container', attrib={'name': 'entry'} )
-                cur_elm = entry_cont
+                if i_s < len( sense_level_tokens ):
 
-            # TODO implement other levels, ENTRY and SENSE levels here
+                    token_lvl3 = sense_level_tokens[i_s][i_st]
+                    token_lvl3_text = token_lvl3[4][6:]
 
-            cur_elm.append( token_r )
+                    if token_r.text == token_lvl3_text:
+                        label_cur[2] = sense_level_labels[i_s][i_st]
+                        i_st += 1
+                        if i_st == len( sense_level_labels[i_s] ):
+                            i_s += 1
+                            i_st = 0
 
+        token_labels.append( label_cur )
         i_pt += 1
 
+    # # doc_elm = ET.Element( 'document' )
+    body_elm = ET.Element( 'body' )
+    cur_elm_lvl1 = None
+    cur_elm_lvl2 = None
+    cur_elm_lvl3 = None
+    labels_prev = ['', '', '']
 
-    xml_string = ET.tostring( doc_elm, encoding='unicode', method='xml' )
+    for i, token_r in enumerate( tokens_raw ):
+
+        lbl_lvl1 = token_labels[i][0]
+        lbl_lvl2 = token_labels[i][1]
+        lbl_lvl3 = token_labels[i][2]
+
+        if lbl_lvl1 == 'SCRAP':
+            if cur_elm_lvl1 is not None and cur_elm_lvl1.tag == 'container' and cur_elm_lvl1.attrib['name'] == 'dictScrap':
+                cur_elm_lvl1.append( token_r )
+                continue
+            else:
+                cur_elm_lvl1 = ET.SubElement( body_elm, 'container', attrib={'name' : 'dictScrap'} )
+                cur_elm_lvl1.append( token_r )
+                continue
+
+        elif 'ENTRY' in lbl_lvl1:
+
+            if lbl_lvl1 == 'ENTRY_START' or cur_elm_lvl1 is None or (cur_elm_lvl1.tag == 'container' and cur_elm_lvl1.attrib['name'] != 'entry'):
+                cur_elm_lvl1 = ET.SubElement( body_elm, 'container', attrib={'name': 'entry'} )
+
+            if lbl_lvl2 == 'INSIDE':
+                cur_elm_lvl1.append( token_r )
+                continue
+
+            elif lbl_lvl2 == 'FORM':
+                if labels_prev[1] != 'FORM':
+                    cur_elm_lvl2 = ET.SubElement( cur_elm_lvl1, 'container', attrib={'name': 'form'} )
+                cur_elm_lvl2.append( token_r )
+                continue
+
+            elif lbl_lvl2 == 'POS':
+                if labels_prev[1] != 'POS':
+                    cur_elm_lvl2 = ET.SubElement( cur_elm_lvl1, 'container', attrib={'name': 'pos'} )
+                cur_elm_lvl2.append( token_r )
+                continue
+
+            elif 'SENSE' in lbl_lvl2:
+
+                if lbl_lvl2 == 'SENSE_START' or cur_elm_lvl2 is None or (cur_elm_lvl2.tag == 'container' and cur_elm_lvl2.attrib['name'] != 'sense'):
+                    cur_elm_lvl2 = ET.SubElement( cur_elm_lvl1, 'container', attrib={'name': 'sense'} )
+
+                if lbl_lvl3 == 'INSIDE':
+                    cur_elm_lvl2.append( token_r )
+                    continue
+                elif lbl_lvl3 == 'TRANS':
+                    if labels_prev[2] != 'TRANS':
+                        cur_elm_lvl3 = ET.SubElement( cur_elm_lvl2, 'container', attrib={'name': 'trans'} )
+                    cur_elm_lvl3.append( token_r )
+
+
+        labels_prev = token_labels[i]
+
+
+
+
+    # label_prev = ""
+    # i_p = 0
+    # i_e = 0
+    # i_s = 0
+    # i_pt = 0
+    # prev_page = int( tokens_raw[0].attrib['page'] )
+    # for token_r in tokens_raw:
+    #
+    #     if token_r.text is None:        # empty tokens were skipped
+    #         continue
+    #
+    #     cur_page = int( token_r.attrib['page'] )
+    #
+    #     if prev_page != cur_page:
+    #         i_p += 1
+    #         i_pt = 0
+    #         prev_page = cur_page
+    #
+    #     token_lvl1 = page_level_tokens[i_p][i_pt]
+    #     token_lvl1_text = token_lvl1[4][6:]
+    #
+    #     if token_r.text != token_lvl1_text:
+    #         print( "error! misaligned data!" )
+    #         break
+    #
+    #     label_cur = page_level_labels[i_p][i_pt]
+    #
+    #     if label_cur == 'SCRAP':
+    #         if cur_elm.tag == 'container' and cur_elm.attrib['name'] == 'dictScrap':
+    #             cur_elm.append( token_r )
+    #         else:
+    #             scrap_cont = ET.SubElement( cur_elm, 'container', attrib={'name' : 'dictScrap'} )
+    #             cur_elm = scrap_cont
+    #             cur_elm.append( token_r )
+    #
+    #     elif label_cur == 'ENTRY_START':
+    #         entry_cont = ET.SubElement( body_elm, 'container', attrib={'name' : 'entry'} )
+    #         cur_elm = entry_cont
+    #         cur_elm.append( token_r )
+    #
+    #         # first element of entry level predictions should be here
+    #         token_lvl2 = entry_level_tokens[i_e][0]
+    #         label_lvl2 = entry_level_labels[i_e][0]
+    #         cur_elm_lvl1 = cur_elm
+    #         if label_lvl2 == "FORM":
+    #             form_cont = ET.SubElement( cur_elm_lvl1, 'container', attrib={'name': 'form'} )
+    #         elif label_lvl2 == 'POS':
+    #             pos_cont = ET.SubElement( cur_elm_lvl1, 'container', attrib={'name': 'pos'} )
+    #
+    #
+    #     elif label_cur == 'ENTRY_INSIDE':
+    #         if cur_elm.tag != 'container' or (cur_elm.tag == 'container' and cur_elm.attrib['name'] != 'entry'):
+    #             entry_cont = ET.SubElement( body_elm, 'container', attrib={'name': 'entry'} )
+    #             cur_elm = entry_cont
+    #
+    #         # TODO implement other levels, ENTRY and SENSE levels here
+    #
+    #         cur_elm.append( token_r )
+    #
+    #     label_prev = label_cur
+    #     i_pt += 1
+
+
+    xml_string = ET.tostring( body_elm, encoding='unicode', method='xml' )
     with open( xml_out_file, 'w' ) as f:
         f.write( xml_string )
 
@@ -83,9 +212,9 @@ def json2xml( json_in_file, xml_raw, xml_out_file ):
 
 if __name__ == "__main__":
 
-    json_ml_results_file = '/media/jan/Fisk/CJVT/outputs/json/irish_trained.json'
-    xml_raw_file = '/media/jan/Fisk/CJVT/data/dicts_xml_november/IrishSample_20p.xml'
-    xml_out_file = '/media/jan/Fisk/CJVT/outputs/json/irish_out.xml'
+    json_ml_results_file = '/media/jan/Fisk/CJVT/outputs/json/predicted_data/mali_sloang_trained_4.json'
+    xml_raw_file = '/media/jan/Fisk/CJVT/data/dicts_xml_december/slovarji/mali_sloang_pred_prelomom-20-pages.xml'
+    xml_out_file = '/media/jan/Fisk/CJVT/outputs/json/predicted_data/mali_sloang_out.xml'
 
     json2xml( json_ml_results_file, xml_raw_file, xml_out_file )
 
